45327dd833ed6ec547a4d0f694a88ade83c83841
Agustin Delmonti <agus.delmonti@gmail.com>
2022-03-29 21:35:03 -0300

    wip: get jwt tokens auth form

--- components/login/LoginForm.tsx
+++ components/login/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import * as Yup from "yup";
 import { Field, Form, Formik } from "formik";
 
@@ -17,59 +17,105 @@ const initialValues = {
 };
 
 function LoginForm() {
+	const [state, setState] = useState({
+		loading: false,
+		response: null,
+		error: null,
+	});
+
 	return (
-		<Formik
-			initialValues={initialValues}
-			onSubmit={values => window.alert(JSON.stringify(values, null, 4))}
-			validationSchema={loginSchema}
-		>
-			{({ values, touched, errors }) => (
-				<Form>
-					<Field
-						id="email"
-						name="email"
-						label="Email"
-						as={TextField}
-						fullWidth
-						value={values.email}
-						error={touched.email && errors.email}
-						helperText={touched.email && errors.email}
-						margin="dense"
-						InputProps={{
-							endAdornment: (
-								<InputAdornment position="end">
-									<AlternateEmailIcon fontSize="small" />
-								</InputAdornment>
-							),
-						}}
-					/>
+		<div>
+			<div>
+				{state.error && "Error"}
+				{state.loading && <div>Loading...</div>}
+			</div>
+			<Formik
+				initialValues={initialValues}
+				onSubmit={({ email, password }) => {
+					setState({ ...state, loading: true });
+					const body = { username: email, password };
+					window.alert(JSON.stringify(body));
 
-					<Field
-						id="password"
-						name="password"
-						label="Password"
-						type="password"
-						as={TextField}
-						fullWidth
-						value={values.password}
-						error={touched.password && errors.password}
-						helperText={touched.password && errors.password}
-						margin="dense"
-						InputProps={{
-							endAdornment: (
-								<InputAdornment position="end">
-									<LockIcon fontSize="small" />
-								</InputAdornment>
-							),
-						}}
-					/>
+					fetch("http://localhost:8080/api/login", {
+						method: "POST",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify(body),
+					})
+						.then(res => res.json())
+						.then(res => {
+							window.alert(JSON.stringify(res));
+							if (res.status !== 200) {
+								window.alert("!!!!!!!!!!!!!!!!	");
+								throw new Error(res.status);
+							}
+							return res;
+						})
+						.then(response =>
+							setState({
+								loading: false,
+								response,
+								error: null,
+							})
+						)
+						.catch(error =>
+							setState({
+								loading: false,
+								response: null,
+								error,
+							})
+						)
+						.finally(() => setState({ ...state, loading: false }));
+				}}
+				validationSchema={loginSchema}
+			>
+				{({ values, touched, errors }) => (
+					<Form>
+						<Field
+							id="email"
+							name="email"
+							label="Email"
+							as={TextField}
+							fullWidth
+							value={values.email}
+							error={touched.email && errors.email}
+							helperText={touched.email && errors.email}
+							margin="dense"
+							InputProps={{
+								endAdornment: (
+									<InputAdornment position="end">
+										<AlternateEmailIcon fontSize="small" />
+									</InputAdornment>
+								),
+							}}
+						/>
 
-					<Button color="primary" variant="contained" type="submit">
-						Iniciar Sesión
-					</Button>
-				</Form>
-			)}
-		</Formik>
+						<Field
+							id="password"
+							name="password"
+							label="Password"
+							type="password"
+							as={TextField}
+							fullWidth
+							value={values.password}
+							error={touched.password && errors.password}
+							helperText={touched.password && errors.password}
+							margin="dense"
+							InputProps={{
+								endAdornment: (
+									<InputAdornment position="end">
+										<LockIcon fontSize="small" />
+									</InputAdornment>
+								),
+							}}
+						/>
+
+						<Button color="primary" variant="contained" type="submit">
+							Iniciar Sesión
+						</Button>
+					</Form>
+				)}
+			</Formik>
+		</div>
 	);
 }
 
