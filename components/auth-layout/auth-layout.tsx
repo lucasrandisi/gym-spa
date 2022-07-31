c209ddd662b98e2aa36d96e6544c24304bb3afca
Agustin Delmonti <agus.delmonti@gmail.com>
2022-07-31 00:16:55 -0300

    add appbar and user actions
    - added user actions in appbar
    - notifications action icon
    - sign out action
    - user profile menu

--- components/auth-layout/auth-layout.tsx
+++ components/auth-layout/auth-layout.tsx
@@ -1,14 +1,133 @@
-import { Box } from "@mui/system";
 import React, { ReactNode } from "react";
-import Sidenav from "../nav/Nav";
+import Link from "next/link";
+
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+import ListItemIcon from "@mui/material/ListItemIcon";
+import { Menu, MenuItem, Tooltip, Avatar, Badge, Divider } from "@mui/material";
+import { FitnessCenter, Logout, Notifications } from "@mui/icons-material";
+import { settings } from "components/nav/NavRoutes";
+import { useAuth } from "security/auth.context";
+import SideNavMenu from "components/nav/SideMenu";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
+	const auth = useAuth();
+	const [open, setOpen] = React.useState(false);
+	const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+	const handleDrawerToggle = () => {
+		setOpen(prev => !prev);
+	};
+
+	const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorElUser(event.currentTarget);
+	};
+
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
+
 	return (
-		<Box sx={{ display: "flex", minHeight: "100vh" }}>
-            <Sidenav />
-            <div style={{ flexGrow: 1, padding: "1rem" }}>
-			    {children}
-            </div>
+		<Box sx={{ display: "flex" }}>
+			<AppBar
+				position="fixed"
+				elevation={0}
+				sx={{ zIndex: theme => theme.zIndex.drawer + 1 }}>
+				<Toolbar>
+					<Box sx={{ display: "flex", alignItems: "center" }}>
+						<IconButton
+							color="inherit"
+							aria-label="toggle open drawer"
+							onClick={handleDrawerToggle}
+							edge="start"
+							sx={{ mr: 1.5 }}>
+							<MenuIcon />
+						</IconButton>
+
+						<FitnessCenter sx={{ display: { xs: "none", md: "flex" }, mr: 0.5 }} />
+						<Link href="/" passHref>
+							<Typography
+								variant="h6"
+								noWrap
+								component="a"
+								href="/"
+								sx={{
+									mr: 2,
+									display: { xs: "none", md: "flex" },
+									fontFamily: "fantasy",
+									fontWeight: 400,
+									letterSpacing: ".1rem",
+									color: "inherit",
+									textDecoration: "none",
+								}}>
+								FitnessWorld
+							</Typography>
+						</Link>
+					</Box>
+
+					<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }} />
+
+					<Box sx={{ flexGrow: 0 }}>
+						<IconButton
+							size="large"
+							aria-label="notifications"
+							color="inherit"
+							sx={{ mr: 1.5 }}>
+							<Badge badgeContent={0} color="error">
+								<Notifications />
+							</Badge>
+						</IconButton>
+
+						<Tooltip title="Open settings">
+							<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+								<Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+							</IconButton>
+						</Tooltip>
+						<Menu
+							sx={{ mt: "45px" }}
+							id="menu-appbar"
+							anchorEl={anchorElUser}
+							anchorOrigin={{
+								vertical: "top",
+								horizontal: "right",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "right",
+							}}
+							open={Boolean(anchorElUser)}
+							onClose={handleCloseUserMenu}>
+							{settings.map(setting => (
+								<Link key={setting.id} href={setting.path} passHref>
+									<MenuItem onClick={handleCloseUserMenu}>
+										<ListItemIcon sx={{ mr: 0.5 }}>{setting.icon}</ListItemIcon>
+										<Typography textAlign="center">{setting.title}</Typography>
+									</MenuItem>
+								</Link>
+							))}
+							<Divider />
+							<MenuItem onClick={auth.logout}>
+								<ListItemIcon sx={{ mr: 0.5 }}>
+									<Logout />
+								</ListItemIcon>
+								<Typography textAlign="center">Log out</Typography>
+							</MenuItem>
+						</Menu>
+					</Box>
+				</Toolbar>
+			</AppBar>
+
+			<SideNavMenu open={open} />
+
+			<Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+				<div style={{ padding: "1rem" }}>{children}</div>
+			</Box>
 		</Box>
 	);
 }
---
+++ components/nav/SideMenu.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import Link from "next/link";
+
+import { styled, Theme, CSSObject } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { routes } from "components/nav/NavRoutes";
+import { useRouter } from "next/router";
+
+const drawerWidth = 160;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+	width: drawerWidth,
+	transition: theme.transitions.create("width", {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.enteringScreen,
+	}),
+	overflowX: "hidden",
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+	transition: theme.transitions.create("width", {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.leavingScreen,
+	}),
+	overflowX: "hidden",
+	width: `calc(${theme.spacing(7)} + 1px)`,
+	[theme.breakpoints.up("sm")]: {
+		width: `calc(${theme.spacing(8)} + 1px)`,
+	},
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "flex-end",
+	padding: theme.spacing(0, 1),
+	// necessary for content to be below app bar
+	...theme.mixins.toolbar,
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: prop => prop !== "open" })(
+	({ theme, open }) => ({
+		width: drawerWidth,
+		flexShrink: 0,
+		whiteSpace: "nowrap",
+		boxSizing: "border-box",
+		...(open && {
+			...openedMixin(theme),
+			"& .MuiDrawer-paper": openedMixin(theme),
+		}),
+		...(!open && {
+			...closedMixin(theme),
+			"& .MuiDrawer-paper": closedMixin(theme),
+		}),
+	})
+);
+
+const SideNavMenu = ({ open }: { open: boolean }) => {
+	const router = useRouter();
+
+	return (
+		<Drawer variant="permanent" open={open}>
+			<DrawerHeader />
+
+			<Box sx={{ flexGrow: 2, display: { xs: "none", md: "flex" } }}>
+				<List>
+					{routes.map(route => (
+						<Link key={route.id} href={route.path} passHref>
+							<ListItem disablePadding sx={{ display: "block" }}>
+								<ListItemButton
+									selected={router.pathname === route.path}
+									sx={{
+										minHeight: 48,
+										justifyContent: open ? "initial" : "center",
+										px: 2.5,
+									}}>
+									<ListItemIcon
+										sx={{
+											minWidth: 0,
+											mr: open ? 3 : "auto",
+											justifyContent: "center",
+										}}>
+										{route.icon}
+									</ListItemIcon>
+									<ListItemText primary={route.title} sx={{ opacity: open ? 1 : 0 }} />
+								</ListItemButton>
+							</ListItem>
+						</Link>
+					))}
+				</List>
+			</Box>
+		</Drawer>
+	);
+};
+
+export default SideNavMenu;
