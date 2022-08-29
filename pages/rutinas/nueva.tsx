b3a7362b5dfcecccaf0733acf0ed8ec9c4258be9
Lucas Randisi <lrandisi@kodear.net>
2022-08-29 00:47:22 -0300

    Routine fields

--- pages/rutinas/nueva.tsx
+++ pages/rutinas/nueva.tsx
@@ -2,13 +2,16 @@ import { Box, Snackbar } from "@mui/material";
 import AuthLayout from "components/auth-layout/auth-layout";
 import Header from "components/header/header";
 import { RoutineForm, RoutineFormType } from "components/routines/RoutinesForm";
+import { Exercise } from "models/exercise";
 import { NextApiRequest } from "next";
 import { useRouter } from "next/router";
 import React, { ReactElement, useState } from "react";
 import { api } from "services/api";
 
 
-const NewRoutine: any = () => {
+
+
+const NewRoutine: any = ({exercises} : {exercises: Array<Exercise>}) => {
 	const router = useRouter()
 	const [openSnackbar, setOpenSnackbar] = useState(false);
 
@@ -31,7 +34,8 @@ const NewRoutine: any = () => {
 		<>
 			<Header title="Rutinas" />
 			<Box sx={{ display: "flex", justifyContent: "center" }}>
-				<RoutineForm
+                <RoutineForm
+                    exercises={exercises}
 					initialValues={initialValues}
 					onSubmit={onSubmit}
 				/>
@@ -47,8 +51,15 @@ const NewRoutine: any = () => {
 
 
 export async function getServerSideProps({ req }: { req: NextApiRequest }) {
-	return {
-		props: {
+    const exercisesResponse = await api.get('/api/exercises', {
+        headers: {
+            Authorization: "Bearer " + req.cookies.access_token
+        }
+    });
+
+    return {
+        props: {
+            exercises: exercisesResponse.data,
 			isProtected: true,
 			userTypes: ["admin"],
 		}
