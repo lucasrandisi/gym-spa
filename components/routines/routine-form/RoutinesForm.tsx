900add8eaaeaeb72789b643b73b15e4b51515905
Lucas Randisi <lucasrandisi@gmail.com>
2022-09-20 17:36:09 -0300

    Verify required fields

--- components/routines/routine-form/RoutinesForm.tsx
+++ components/routines/routine-form/RoutinesForm.tsx
@@ -6,17 +6,7 @@ import { RoutineExercise, RoutineExercisesTable } from "../routine-exercises-tab
 import styles from './routine-form.module.scss';
 import * as yup from 'yup';
 
-const Form = styled('form')({
-	width: "30%",
-});
-
-const validationSchema = yup.object({
-    name: yup.string().required(),
-    exercise: yup.object().required(),
-    day: yup.number().required(),
-    sets: yup.number().required(),
-    reps: yup.number().required(),
-})
+const Form = styled('form')({});
 
 export type RoutineForm = {
     name: string,
@@ -38,8 +28,7 @@ type RoutineFormProps = {
 const dias = [1, 2, 3, 4, 5, 6, 7];
 
 export function RoutineForm({ exercises, onSubmit, routineExercises }: RoutineFormProps) {
-    const [selectedExercise, setSelectedExercise] = useState<Exercise | null>(null);
-    const [newRoutineExercises, setNewRoutneExercises] = useState(routineExercises.map(routineExercise => {
+    const [newRoutineExercises, setNewRoutineExercises] = useState(routineExercises.map(routineExercise => {
         return {
             key: new Date().getTime(),
             exercise: routineExercise.exercise,
@@ -49,28 +38,48 @@ export function RoutineForm({ exercises, onSubmit, routineExercises }: RoutineFo
         }
     }));
 
-    const formik = useFormik({
+    const routineFormik = useFormik({
         initialValues: {
-            name: "",
-            day: "",
-            sets: "",
-            reps: "",
+            name: undefined,
         },
-        onSubmit: onSubmit,
-        validationSchema: validationSchema
-
+        validationSchema: yup.object({
+            name: yup.string().required(),
+        }),
+        onSubmit: onSubmit
     });
 
+    const routineExerciseFormik = useFormik({
+        initialValues: {
+            exercise: null,
+            day: '',
+            sets: '',
+            reps: ''
+        },
+        validationSchema: yup.object({
+            exercise: yup.object().required(),
+            day: yup.number().required(),
+            sets: yup.number().required(),
+            reps: yup.number().required()
+        }),
+        onSubmit: () => {}
+    })
 
-    function addExercise() {
-        setNewRoutneExercises([
+
+    async function addExercise() {
+        const errors = await routineExerciseFormik.validateForm();
+
+        if (Object.keys(errors).length !== 0) {
+            return;
+        }
+
+        setNewRoutineExercises([
             ...newRoutineExercises,
             {
                 key: new Date().getTime(),
-                exercise: selectedExercise!,
-                day: Number(formik.values.day),
-                sets: Number(formik.values.sets),
-                reps: Number(formik.values.reps),
+                exercise: routineExerciseFormik.values.exercise!,
+                day: Number(routineExerciseFormik.values.day),
+                sets: Number(routineExerciseFormik.values.sets),
+                reps: Number(routineExerciseFormik.values.reps),
             }
         ].sort((a, b) => {
             if (a.day < b.day) {
@@ -80,102 +89,100 @@ export function RoutineForm({ exercises, onSubmit, routineExercises }: RoutineFo
             } else {
                 return a.key > b.key ? 1 : -1;
             }
-        }))
+        }));
     }
-
+    
     function removeRoutineExercise(key: number) {
-        setNewRoutneExercises(newRoutineExercises.filter(newRoutineExercise => newRoutineExercise.key != key));
+        setNewRoutineExercises(newRoutineExercises.filter(newRoutineExercise => newRoutineExercise.key != key));
     }
-
+    
     return (
-        <Box sx={{width: "80%"}}>
-            <Form onSubmit={formik.handleSubmit} sx={{width: "100%", display: "flex", flexDirection: "column"}}>
-                <TextField
-                    name="name"
-                    label="Nombre"
-                    value={formik.values.name}
-                    onChange={formik.handleChange}
-                    error={formik.touched.name && Boolean(formik.errors.name)}
-                    helperText={formik.touched.name && formik.errors.name}
-                    sx={{ width: "30%", mb: 2 }}
+        <Form
+            onSubmit={routineFormik.handleSubmit}
+            sx={{ width: "100%", display: "flex", flexDirection: "column" }}>
+            <TextField
+                name="name"
+                label="Nombre"
+                value={routineFormik.values.name}
+                onChange={routineFormik.handleChange}
+                error={Boolean(routineFormik.errors.name)}
+                sx={{ width: "25%", mb: 6 }}
+            />
+
+            <Box sx={{ display: 'flex', alignItems: "center", width: '100%', mb: 3 }}>
+                <Autocomplete
+                    options={exercises}
+                    getOptionLabel={(exercise) => exercise.name}
+                    onChange={(_, value) => routineExerciseFormik.setFieldValue("exercise", value)}
+                    autoSelect
+                    autoHighlight
+                    sx={{ width: "25%", mr: 2 }}
+                    renderInput={(params) =>
+                        <TextField
+                            {...params}
+                            error={Boolean(routineExerciseFormik.errors.exercise)}
+                            label="Ejercicio"
+                            type="outlined"
+                        />
+                    }
                 />
-                <Box sx={{ display: 'flex', alignItems: "center", width: '100%', mb: "1rem"}}>
-                    <Autocomplete
-                        options={exercises}
-                        getOptionLabel={(exercise) => exercise.name}
-                        autoSelect
-                        autoHighlight
-                        sx={{ width: "25%", mr: 2 }}
-                        renderInput={(params) =>
-                            <TextField
-                                {...params}
-                                label="Ejercicio"
-                                type="outlined"
-                            />
-                        }
-                    />
-                    <FormControl sx={{ width: "7%", mr: 2 }}>
-                        <InputLabel>Día</InputLabel>
-                        <Select
-                            name="day"
-                            value={formik.values.day}
-                            onChange={formik.handleChange}
-                            error={formik.touched.sets && Boolean(formik.errors.sets)}
-                            input={<OutlinedInput label="Día" />}>
-                            {dias.map((dia: any) => (
-                                <MenuItem key={dia} value={dia}>
-                                    {dia}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                    <TextField
-                        name="sets"
-                        label="Sets"
-                        value={formik.values.sets}
-                        onChange={formik.handleChange}
-                        error={formik.touched.sets && Boolean(formik.errors.sets)}
-                        type="number"
-                        sx={{ width: "7%", mr: 2 }}
-                        className={styles.numberInput}
-                    />
-                    <TextField
-                        name="reps"
-                        label="Reps"
-                        value={formik.values.reps}
-                        onChange={formik.handleChange}
-                        error={formik.touched.reps && Boolean(formik.errors.reps)}
-                        type="number"
-                        sx={{ width: "7%", mr: 2 }}
-                        className={styles.numberInput}
-                    />
-                    <Button
-                        color="primary"
-                        size="medium"
-                        variant="contained"
-                        type="submit"
-                        sx={{ width: "10%", height: "100%" }}
-                    >
-                        Agregar
-                    </Button>
-                </Box>
-            </Form>
+                <FormControl sx={{ width: "7%", mr: 2 }}>
+                    <InputLabel>Día</InputLabel>
+                    <Select
+                        name="day"
+                        value={routineExerciseFormik.values.day}
+                        onChange={routineExerciseFormik.handleChange}
+                        error={Boolean(routineExerciseFormik.errors.day)}
+                        input={<OutlinedInput label="Día" />}>
+                        {dias.map((dia: any) => (
+                            <MenuItem key={dia} value={dia}>
+                                {dia}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                <TextField
+                    name="sets"
+                    label="Sets"
+                    value={routineExerciseFormik.values.sets}
+                    onChange={routineExerciseFormik.handleChange}
+                    error={Boolean(routineExerciseFormik.errors.sets)}
+                    type="number"
+                    sx={{ width: "7%", mr: 2 }}
+                    className={styles.numberInput}
+                />
+                <TextField
+                    name="reps"
+                    label="Reps"
+                    value={routineExerciseFormik.values.reps}
+                    onChange={routineExerciseFormik.handleChange}
+                    error={Boolean(routineExerciseFormik.errors.reps)}
+                    type="number"
+                    sx={{ width: "7%", mr: 2 }}
+                    className={styles.numberInput}
+                />
+                <Button
+                    color="primary"
+                    size="medium"
+                    variant="contained"
+                    onClick={() => addExercise()}
+                    sx={{ width: "10%", height: "100%" }}>
+                    Agregar
+                </Button>
+            </Box>
 
             <RoutineExercisesTable
                 routineExercises={newRoutineExercises}
                 removeRoutineExercise={removeRoutineExercise}
-            />
+                />
 
             <Box sx={{display: "flex", justifyContent: "center"}}>
                 <Button
-                    onClick={() => onSubmit({
-                        name: formik.values.name,
-                        newRoutineExercises: newRoutineExercises
-                    })}
+                    type="submit"
                     color="primary" variant="contained" sx={{ width: "20%" }}>
                     Guardar
                 </Button>
             </Box>
-        </Box>
-	);
+        </Form>
+    );
 }
