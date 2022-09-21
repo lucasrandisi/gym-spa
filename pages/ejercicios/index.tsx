79bcc28c44cb1703be65d9f726e730f4531010a5
Lucas Randisi <lucasrandisi@gmail.com>
2022-09-21 14:36:52 -0300

    Copy table style on exercises page

--- pages/ejercicios/index.tsx
+++ pages/ejercicios/index.tsx
@@ -1,101 +1,126 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import AuthLayout from "components/auth-layout/auth-layout";
 import Header from "components/header/header";
+import { NextApiRequest } from "next/types";
 import React, { ReactElement } from "react";
-import Link from "next/link";
-import Box from "@mui/system/Box";
-import Button from "@mui/material/Button";
-import ExerciseService from "services/exercise.service";
-import { useQuery } from "@tanstack/react-query";
-import DebouncedInput from "components/debounced";
-import {
-	ColumnDef,
-	createColumnHelper,
-	getCoreRowModel,
-	getFilteredRowModel,
-	getPaginationRowModel,
-	useReactTable,
-} from "@tanstack/react-table";
-import { Exercise } from "models/exercise";
-import DataTable from "components/table/Table";
-import ExerciseTableActions from "components/exercise/Actions";
-import fuzzyFilter from "../../components/table/fuzzyFilter";
+import { api } from "services/api";
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
+import Link from 'next/link';
+import TextField from '@mui/material/TextField';
+import { Box } from '@mui/system';
+import { Button, Snackbar } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Exercise } from 'models/exercise';
 
-const columnHelper = createColumnHelper<Exercise>();
 
-const columns: ColumnDef<Exercise, any>[] = [
-	columnHelper.accessor("id", {
-		header: "ID",
-		cell: info => info.getValue(),
-	}),
-	columnHelper.accessor("name", {
-		header: "Nombre",
-		cell: info => info.getValue(),
-	}),
-	{
-		id: "actions",
-		header: "Acciones",
-		cell: props => <ExerciseTableActions row={props.row} />,
-	},
-];
 
-const ExercisesPage: any = () => {
-	const [globalFilter, setGlobalFilter] = React.useState("");
+const ExercisesPage: any = ({ exercisesList }: { exercisesList: Array<Exercise> }) => {
+    const [name, setName] = React.useState('');
+    const [exercises, setExercises] = React.useState(exercisesList);
+    const [openDeleteSnackbar, setOpenDeleteSnackbar] = React.useState(false);
 
-	const { isLoading, error, data } = useQuery(["excercises"], ExerciseService.getAll, {
-		initialData: [],
-	});
+    const onSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const searchValue = event.target.value;
+        const filteredExercises = exercisesList.filter(exercise =>
+            exercise.name.toLocaleLowerCase().includes(searchValue));
 
-	const table = useReactTable({
-		data,
-		columns,
-		globalFilterFn: fuzzyFilter,
-		state: {
-			globalFilter,
-		},
-		getCoreRowModel: getCoreRowModel(),
-		getFilteredRowModel: getFilteredRowModel(),
-		getPaginationRowModel: getPaginationRowModel(),
-		enableColumnFilters: true,
-	});
+        setName(searchValue);
+        setExercises(filteredExercises);
+    };
 
-	if (isLoading) return "Loading...";
+    async function deleteExercise(id: number) {
+        await api.delete(`/api/exercises/${id}`);
+        setOpenDeleteSnackbar(true);
 
-	if (error) return `An error has occurred: ${(error as Error).message}`;
+        api.get(`api/exercises`).then(({ data }) => setExercises(data));
+    }
 
-	return (
-		<div>
-			<Header title="Ejercicios" />
-			<Box sx={{ mb: 4, px: 2, display: "flex", alignItems: "flex-end" }}>
-				<DebouncedInput
-					value={globalFilter}
-					onChange={value => setGlobalFilter(String(value))}
-					placeholder="Buscar..."
-					type="search"
-				/>
+    return (
+        <>
+            <Header title="Ejercicios" />
+            <Box sx={{ mb: 4, px: 2, display: "flex", alignItems: "flex-end" }}>
+                <TextField
+                    label="Buscar Ejercicio"
+                    variant="standard"
+                    type="search"
+                    value={name}
+                    onChange={onSearchChange}
+                    color="secondary"
+                />
+                <Box sx={{ ml: "auto" }}>
+                    <Link href="ejercicios/nuevo">
+                        <Button variant="contained">Agregar</Button>
+                    </Link>
+                </Box>
+            </Box>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Id</TableCell>
+                            <TableCell>Nombre</TableCell>
+                            <TableCell />
+                            <TableCell />
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {exercises.map((exercise) => (
+                            <TableRow key={exercise.id}>
+                                <TableCell>{exercise.id}</TableCell>
+                                <TableCell>{exercise.name}</TableCell>
+                                <TableCell>
+                                    <Link href={`/ejercicios/${exercise.id}`}>
+                                        <IconButton aria-label="edit">
+                                            <EditIcon />
+                                        </IconButton>
+                                    </Link>
+                                </TableCell>
+                                <TableCell>
+                                    <IconButton onClick={() => deleteExercise(exercise.id)} aria-label="delete">
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Snackbar
+                open={openDeleteSnackbar}
+                message="Ejercicio eliminado"
+                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+            />
+        </>
+    );
 
-				<Box sx={{ ml: "auto" }}>
-					<Link href="ejercicios/nuevo" passHref>
-						<Button variant="contained">Agregar</Button>
-					</Link>
-				</Box>
-			</Box>
+}
 
-			<DataTable table={table} />
-		</div>
-	);
-};
+
+export async function getServerSideProps({ req }: { req: NextApiRequest }) {
+    const exercisesResponse = await api.get('/api/exercises', {
+        headers: {
+            Authorization: "Bearer " + req.cookies.access_token
+        }
+    });
+
+    return {
+        props: {
+            exercisesList: exercisesResponse.data,
+            isProtected: true,
+            userTypes: ["admin"],
+        }
+    };
+}
 
 export default ExercisesPage;
 
-export async function getStaticProps() {
-	return {
-		props: {
-			isProtected: true,
-			userTypes: ["admin"],
-		},
-	};
-}
-
 ExercisesPage.getLayout = function getLayout(page: ReactElement) {
-	return <AuthLayout>{page}</AuthLayout>;
+    return <AuthLayout>{page}</AuthLayout>;
 };
