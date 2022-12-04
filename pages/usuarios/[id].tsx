d1c81db35dbf62ed63739f62c3f5cf736201ed05
Agustin Delmonti <A446534@santandertecnologia.com.ar>
2022-12-04 12:40:51 -0300

    delete routine field when creating user

--- pages/usuarios/[id].tsx
+++ pages/usuarios/[id].tsx
@@ -1,97 +1,92 @@
 import { Box, Snackbar } from "@mui/material";
 import AuthLayout from "components/auth-layout/auth-layout";
 import Header from "components/header/header";
-import { UserForm } from "components/users/UserForm";
+import { UserForm, UserFormType } from "components/users/UserForm";
 import { Rol } from "models/rol";
-import { Routine } from "models/routine";
 import { User } from "models/user";
 import { NextApiRequest } from "next";
 import { useRouter } from "next/router";
 import React, { ReactElement, useState } from "react";
 import { api } from "services/api";
 
+const EditUser: any = ({ roles, user }: { roles: Rol[]; user: User }) => {
+	const router = useRouter();
+	const [openSnackbar, setOpenSnackbar] = useState(false);
+	const [submitError, setSubmitError] = useState(null);
 
-const EditUser: any = ({ roles, user, routines }: { roles: Rol[], user: User, routines: Routine[] }) => {
-    const router = useRouter()
-    const [openSnackbar, setOpenSnackbar] = useState(false);
-    const [submitError, setSubmitError] = useState(null);
+	const initialValues: UserFormType = {
+		name: user.name,
+		email: user.email,
+		nroDoc: user.nroDoc,
+		rolId: user.roles[0].id,
+	};
 
+	function onSubmit(values: UserFormType) {
+		api
+			.put(`/api/users/${user.id}`, values)
+			.then(() => {
+				setOpenSnackbar(true);
 
-    const initialValues: UserForm = {
-        name: user.name,
-        email: user.email,
-        nroDoc: user.nroDoc,
-        rolId: user.roles[0].id,
-        routineId: user.routine ? user.routine.id : "",
-    };
+				setTimeout(() => router.push("/usuarios"), 2000);
+			})
+			.catch(errorResponse => {
+				setSubmitError(errorResponse.message);
+			});
+	}
 
-    function onSubmit(values: UserForm) {
-        api.put(`/api/users/${user.id}`, values)
-            .then(() => {
-                setOpenSnackbar(true);
+	return (
+		<>
+			<Header title="Actualizar Usuario" />
+			<Box sx={{ display: "flex", justifyContent: "center" }}>
+				<UserForm
+					initialValues={initialValues}
+					onSubmit={onSubmit}
+					roles={roles}
+					passwordInput={false}
+					submitError={submitError}
+				/>
+			</Box>
+			<Snackbar
+				open={openSnackbar}
+				message="Usuario Actualizado"
+				anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+			/>
+		</>
+	);
+};
 
-                setTimeout(() => router.push("/usuarios"), 2000);
-            }).catch((errorResponse) => {
-                setSubmitError(errorResponse.message)
-            });;
-    }
+export async function getServerSideProps({
+	req,
+	params,
+}: {
+	req: NextApiRequest;
+	params: { id: string };
+}) {
+	const [rolesResponse, userResponse] = await Promise.all([
+		api.get("/api/roles", {
+			headers: {
+				Authorization: `Bearer ${req.cookies.access_token}`,
+			},
+		}),
+		api.get(`/api/users/${params.id}`, {
+			headers: {
+				Authorization: `Bearer ${req.cookies.access_token}`,
+			},
+		}),
+	]);
 
-    return (
-        <>
-            <Header title="Actualizar Usuario" />
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-                <UserForm
-                    initialValues={initialValues}
-                    onSubmit={onSubmit}
-                    roles={roles}
-                    routines={routines}
-                    passwordInput={false}
-                    submitError={submitError}
-                />
-            </Box>
-            <Snackbar
-                open={openSnackbar}
-                message="Usuario Actualizado"
-                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-            />
-        </>
-    );
+	return {
+		props: {
+			roles: rolesResponse.data,
+			user: userResponse.data,
+			isProtected: true,
+			userTypes: ["Admin"],
+		},
+	};
 }
-
-
-export async function getServerSideProps({ req, params }: { req: NextApiRequest, params: { id: string } }) {
-    const [rolesResponse, routinesResponse, userResponse] = await Promise.all([
-        api.get('/api/roles', {
-            headers: {
-                Authorization: "Bearer " + req.cookies.access_token
-            }
-        }),
-        api.get('/api/routines', {
-            headers: {
-                Authorization: "Bearer " + req.cookies.access_token
-            }
-        }),
-        api.get(`/api/users/${params.id}`, {
-            headers: {
-                Authorization: "Bearer " + req.cookies.access_token
-            }
-        })
-    ]);
-
-    return {
-        props: {
-            roles: rolesResponse.data,
-            routines: routinesResponse.data,
-            user: userResponse.data,
-            isProtected: true,
-            userTypes: ["Admin"],
-        }
-    };
-}
-
 
 export default EditUser;
 
 EditUser.getLayout = function getLayout(page: ReactElement) {
-    return <AuthLayout>{page}</AuthLayout>;
+	return <AuthLayout>{page}</AuthLayout>;
 };
